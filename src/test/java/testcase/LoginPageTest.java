19b0043c8d4012198846418346bef0a4a2a16244
dieudta <dinhthianhdieu@gmail.com>
2023-08-17 00:02:33 +0700

    creat project with pom

--- src/test/java/testcase/LoginPageTest.java
+++ src/test/java/testcase/LoginPageTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import page.LoginPage;
 
 public class LoginPageTest extends InitDriver {
+
     @Test(dataProvider = "loginDataCsv", dataProviderClass = DataProviderCsv.class, groups = "login")
     public void testcase2_login_to_tracker_csv(String username, String password){
         super.getDriver().get("https://www.browserstack.com/users/sign_in");
