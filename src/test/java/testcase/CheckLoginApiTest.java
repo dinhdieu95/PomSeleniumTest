01a5bc0b134596887db4164305ec54edd4f0b153
dieudta <dinhthianhdieu@gmail.com>
2023-08-20 21:29:25 +0700

    add api check

---
+++ src/test/java/testcase/CheckLoginApiTest.java
@@ -0,0 +1,20 @@
+package testcase;
+
+import common.RequestAPI;
+import common.dto.LoginDTO;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CheckLoginApiTest {
+    @Test
+    public  void check_login(){
+        Map<String, Object> request = new HashMap<>();
+        LoginDTO loginDTO = new LoginDTO("105C056630","12345678");
+        Response response = RequestAPI.post("https://apipub.tcbs.com.vn/authen/v1/login", request, loginDTO);
+        Assert.assertEquals( response.getStatusCode() , 400, "Kết quả không khớp");
+    }
+}
